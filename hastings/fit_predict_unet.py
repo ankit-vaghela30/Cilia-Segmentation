c7fb7a45a7d2bb15322416ea73b01a30145743fb
Vyom Shrivastava <vyom.shrivastava1@gmail.com>
2018-04-03 18:52:30 -0400

    Preprocessing to augment data and code to fit unet
    The file fit_predict_unet.py has code to fit the unet model and to predict from a trained model
    Added augment_data() to  preprocessing which smoothens the image before sending it to the model.

---
+++ hastings/fit_predict_unet.py
@@ -0,0 +1,42 @@
+import numpy as np
+from preprocessing import augment_data
+from unet_model import unet
+import argparse
+
+#Importing keras function
+from keras.models import Model
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose
+from keras.optimizers import Adam
+from keras import backend as K
+
+
+#getting command line argument
+parser = argparse.ArgumentParser(description='paths')
+parser.add_argument('--mode',type=str,help='fit or predict?')
+parser.add_argument('--image_path',type=str,help='path of image npy array')
+parser.add_argument('--mask_path',type=str,help='path of mask npy array')
+parser.add_argument('--model_path',type=str,help='path of saved model')
+parser.add_argument('--save_path',type=str,help='path to save model or prediction')
+args = parser.parse_args()
+
+#Augmenting data
+images,mask = augment_data(args.image_path,args.mask_path)
+
+def fit(training_images,mask_images,save_path):
+    model = unet()
+    #Fitting and saving model
+    model.fit(training_images, mask_images, batch_size=4, epochs=100, verbose=1, shuffle=True)
+    model.save(save_path+"/"+"model.h5")
+
+def predict(testing_images,model_path,save_result_path):
+    #loading model and predicting mask
+    model=unet()
+    model.load_weights(model_path)
+
+    prediction = model.predict(testing_images, verbose=1)
+    np.save(save_result_path+'/prediction.npy', prediction)
+
+if(args.mode=="fit"):
+    fit(images,mask,args.save_path)
+if(args.mode=="predict"):
+    predict(images,args.model_path,args.save_path)
---
+++ hastings/preprocessing.py
@@ -0,0 +1,109 @@
+import cv2
+import numpy as np
+def threshold(image,t):
+    '''
+        Setting threhold to the image. Turn pixels below threshold black.
+        
+        Args:
+            image: the input grayscale image
+                   type: ndarray, shape: (dim1,dim2)
+            t: the threshold
+               type: INT
+        
+        Return:
+            new_image: the transformed image
+        '''
+    _, new_image = cv2.threshold(image,t,255,cv2.THRESH_BINARY)
+    return new_image
+
+def blur(image,b):
+    '''
+        Smoothing the image
+        
+        Args:
+            image: the input image
+                   type: ndarray, shape: (dim1,dim2)
+            b: size of median filter
+            
+        Return:
+            the transformed image
+        '''
+    return cv2.medianBlur(image,b)
+
+def gray2rgb(image):
+    '''
+        Transform grayscale image to RGB image
+        
+        Args:
+            image: the input grayscale image
+                   type: ndarray, shape: (dim1,dim2)
+                   
+        Return:
+            the transformed RGB image
+        '''
+    return cv2.cvtColor(image,cv2.COLOR_GRAY2RGB)
+
+def rgb2gray(image):
+    '''
+        Transform RGB image to grayscale image
+        
+        Args:
+            image: the input RGB image
+                   type: ndarray, shape: (dim1,dim2)
+                   
+        Return:
+            the transformed grayscale image
+        '''
+    return cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+def rgb2hsv(image):
+    '''
+        Transform BGR image to HSV image
+        
+        Args:
+            image: the input RGB image
+                   type: ndarray, shape: (dim1,dim2)
+        
+        Return:
+            the transformed HSV image
+        '''
+    return cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+
+def hsv2rgb(image):
+    '''
+        Transform HSV image to RGB image
+        
+        Args:
+            image: the input HSV image
+                   type: ndarray, shape: (dim1,dim2)
+        
+        Return:
+            the transformed RGB image
+        '''
+    return cv2.cvtColor(image,cv2.COLOR_HSV2BGR)
+
+def augment_data(image_path,mask_path):
+    '''
+    loads images and mask as a numpy array, reshapes images and mask to 4 channels
+    and normalzes image data
+    
+    Args:
+        image_path:the path of the npy array containing images
+                   type: String
+        mask_path:      the path to npy array containing masks
+                   type: String
+    Return:
+        numpy array of augmented mask and image
+    '''
+    image_array = np.load(image_path)
+    mask_array = np.load(mask_path)
+    
+    images = image_array[...,np.newaxis]
+    masks = mask_array[...,np.newaxis]
+
+    image_data_array = images.astype('float32')
+    mask_images = masks.astype('float32')
+    data_images = image_data_array - np.mean(image_data_array)
+    data_images = data_images / np.std(image_data_array)
+
+    return data_images, mask_images
